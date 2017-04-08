13f97e65c211287c654fd505fa32d545119a3e8e
sakura-fly <sourcesakura@sina.com>
2017-04-08 17:37:41 +0800

    再配置jpa

--- src/com/config/DbConfig.java
+++ src/com/config/DbConfig.java
@@ -1,10 +1,50 @@
 package com.config;
 
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jndi.JndiObjectFactoryBean;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import com.util.DbConstanst;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.odyz.dao")
 public class DbConfig {
 
+	@Bean
+	public BasicDataSource dataSource() {
+
+		BasicDataSource ds = new BasicDataSource();
+		ds.setDriverClassName(DbConstanst.DBDRIVER);
+		ds.setUrl(DbConstanst.DBURL);
+		ds.setUsername(DbConstanst.USERNAME);
+		ds.setPassword(DbConstanst.PWD);
+		return ds;
+	}
+
+	@Bean
+	public HibernateJpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(false);
+		adapter.setGenerateDdl(true);
+		return adapter;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(DataSource datasource,
+			HibernateJpaVendorAdapter va) {
+		LocalContainerEntityManagerFactoryBean embf = new LocalContainerEntityManagerFactoryBean();
+		embf.setPersistenceUnitName("odyz");
+		embf.setJpaVendorAdapter(va);
+		embf.setDataSource(datasource);
+		embf.setPackagesToScan("com.odyz.model");
+		return embf;
+	}
 }
---
+++ src/com/util/DbConstanst.java
@@ -0,0 +1,9 @@
+package com.util;
+
+public class DbConstanst {
+	public static String DBDRIVER = "";
+	public static String DBURL = "";
+	public static String USERNAME = "";
+	public static String PWD = "";
+	
+}
